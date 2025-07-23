2e540c02bf671ac863b9ab3cdf06f294ebd25a0b
samar_gharieb <samargharieb1993@gmail.com>
2025-07-23 13:10:29 +0300

    commit

--- generate_allure_report.bat
+++ generate_allure_report.bat
@@ -1,6 +1,6 @@
 @echo off
 :: If you already have a valid JAVA_HOME environment variable set, feel free to comment the below two lines
-set JAVA_HOME=C:\Program Files\Java\jdk-23
+set JAVA_HOME=C:\Users\HP\.jdks\openjdk-21.0.2
 set path=%JAVA_HOME%\bin;%path%
 set path=C:\Users\HP\.m2\repository\allure\allure-2.33.0\bin;%path%
 allure serve allure-results -h localhost
